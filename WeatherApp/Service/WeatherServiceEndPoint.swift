d046901edef5e7d0b524e3375a45bcf8bc897e91
MustafaGrgn <mustafagorgun87@hotmail.com>
2022-01-20 00:01:34 +0300

    Service End Point Added

--- WeatherApp/Service/WeatherServiceEndPoint.swift
+++ WeatherApp/Service/WeatherServiceEndPoint.swift
@@ -6,4 +6,28 @@
 //
 
 import Foundation
+protocol GenericValueProtocol {
+    associatedtype Value
+    var value: Value { get }
+}
+enum WeatherServiceEndPoint: GenericValueProtocol{
+    typealias Value = String
+
+    var value: String {
+        switch self {
+        case .base:
+            return "https://api.openweathermap.org/data/2.5/onecall?"
+        case .latlong(let size1,let size2):
+            return "lat=\(size1)&lon=\(size2)&"
+        case .exclude(let exclude):
+            return "exclude=\(exclude)&"
+        case .api(let api):
+            return "appid=\(api)"
+        }
+    }
+    case base
+    case latlong(Double,Double)
+    case exclude(String)
+    case api(String)
+}
 
